248ce499d4cb103e4c2faafaa26ca07858b13b50
Jhony-j2ac <jhony.ardila11@gmail.com>
2023-07-21 01:08:15 -0500

    Django api configured

--- build_files.sh
+++ build_files.sh
@@ -2,7 +2,7 @@
 #Este archivo fue generado como parte serverless en vercel, aca puedo configurar para instalar dependencias faltantes
 yum install mysql-devel
 pip install -r requirements.txt
-pip3 install Django==4.2.3
+pip3 install Django==3.0.5
 pip3 install djangorestframework==3.14.0
-pip3 install django-cors-headers==4.2.0
+pip3 install django-cors-headers==3.2.0
 yes | python3.9 manage.py collectstatic
