10c3ca541f675be20a6eea80816d331a02678fd0
Jhony-j2ac <jhony.ardila11@gmail.com>
2023-07-21 00:24:53 -0500

    Django api configured

--- build_files.sh
+++ build_files.sh
@@ -2,4 +2,5 @@
 #Este archivo fue generado como parte serverless en vercel, aca puedo configurar para instalar dependencias faltantes
 pip install -r requirements.txt
 pip3 install Django
+pip3 install djangorestframework
 python3.9 manage.py collectstatic
