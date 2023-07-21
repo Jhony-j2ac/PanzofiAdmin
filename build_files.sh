36d70466a5bbb4e13c31a4b160595b44d1dfdae6
Jhony-j2ac <jhony.ardila11@gmail.com>
2023-07-21 01:09:04 -0500

    Django api configured

--- build_files.sh
+++ build_files.sh
@@ -5,4 +5,4 @@ pip install -r requirements.txt
 pip3 install Django==3.0.5
 pip3 install djangorestframework==3.14.0
 pip3 install django-cors-headers==3.2.0
-yes | python3.9 manage.py collectstatic
+#yes | python3.9 manage.py collectstatic
