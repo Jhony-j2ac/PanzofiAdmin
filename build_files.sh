ff89f7bb9e3d11259bad15da2ed4df47954a44f7
Jhony-j2ac <jhony.ardila11@gmail.com>
2023-07-30 11:56:39 -0500

    configuracion dotenv para pruebas

--- build_files.sh
+++ build_files.sh
@@ -5,6 +5,6 @@ pip install -r requirements.txt
 pip3 install Django==4.2.3
 pip3 install djangorestframework==3.14.0
 pip3 install django-cors-headers==4.2.0
-mkdir -p /vercel/path0/static
-chmod +rwx /vercel/path0/static
-yes | python3.9 manage.py collectstatic
+# mkdir -p /vercel/path0/static
+# chmod +rwx /vercel/path0/static
+# yes | python3.9 manage.py collectstatic
