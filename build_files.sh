131f73464574a7c4d31aa79188bf4f981106b7c6
Jhony-j2ac <jhony.ardila11@gmail.com>
2023-07-30 12:10:33 -0500

    configuracion dotenv para pruebas

--- build_files.sh
+++ build_files.sh
@@ -5,6 +5,7 @@ pip install -r requirements.txt
 pip3 install Django==4.2.3
 pip3 install djangorestframework==3.14.0
 pip3 install django-cors-headers==4.2.0
-# mkdir -p /vercel/path0/static
-# chmod +rwx /vercel/path0/static
-# yes | python3.9 manage.py collectstatic
+pip3 install mysqlclient==2.1.1
+mkdir -p /vercel/path0/static
+chmod +rwx /vercel/path0/static
+yes | python3.9 manage.py collectstatic
